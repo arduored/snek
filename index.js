1f7328385b76d10fe323ea8adca7c63b51de91a0
Edouard Souan <souan.edouard0@gmail.com>
2021-01-31 07:51:57 +0100

    add score, game states and direction control to avoid u-turn on the spot

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 import Snek from "./snek.js"
 import Food from "./food.js"
 
+const score = document.getElementById("score")
+
 document.addEventListener("keyup", inputListener)
 
 const canvas = document.getElementById("game-canvas")
+const screenPause = document.getElementById("pause-screen")
 const ctx = canvas.getContext("2d")
 
 const WIDTH = 800
 const HEIGHT = 600
 const SPEED = 200
 const SIZE = 10
+
+const STATES = {
+    MENU: 0,
+    PLAY: 1,
+    PAUSE: 2,
+    FINISHED: 3,
+}
+
+let GAME_STATE = 0
+let SCORE = 0
 
 const snek = new Snek(WIDTH, HEIGHT, SIZE, SPEED)
 const food = new Food(WIDTH, HEIGHT, SIZE)
@@ -19,42 +32,61 @@ function inputListener(e) {
     switch (e.keyCode) {
         case 87: // W
         case 38:
-            direction.x = 0
-            direction.y = -1
+            if (direction.y !== 1) {
+                direction.x = 0
+                direction.y = -1
+            }
             break
         case 65: // A
         case 37:
-            direction.x = -1
-            direction.y = 0
+            if (direction.x !== 1) {
+                direction.x = -1
+                direction.y = 0
+            }
             break
         case 83: // S
         case 40:
-            direction.x = 0
-            direction.y = 1
+            if (direction.y !== -1) {
+                direction.x = 0
+                direction.y = 1
+            }
             break
         case 68: // D
         case 39:
-            direction.x = 1
-            direction.y = 0
+            if (direction.x !== -1) {
+                direction.x = 1
+                direction.y = 0
+            }
+            break
+        case 32:
+            toggleGameState()
             break
         default:
-            console.warn("Not a supported input")
+            console.warn(`${e.keyCode} is not a supported input`)
     }
 }
 
-let lastTime = 0
-let elapsedTime = 0
+function startGame() {
+    GAME_STATE = 1
+}
 
-function update(timestamp) {
+function refreshScore() {
+    score.innerText = `Score: ${SCORE}`
+}
+
+function play(timestamp) {
     const delta = timestamp - lastTime
     lastTime = timestamp
     elapsedTime += delta
+    refreshScore()
 
     if (elapsedTime > SPEED) {
         ctx.clearRect(0, 0, WIDTH, HEIGHT)
         if (snek.x === food.x && snek.y === food.y) {
             snek.eat(food)
             food.update(WIDTH, HEIGHT, SIZE)
+            SCORE += 10
+            refreshScore()
         }
 
         if (snek.hitBorder(WIDTH, HEIGHT)) {
@@ -68,6 +100,35 @@ function update(timestamp) {
 
         elapsedTime = 0
     }
+}
+
+function toggleGameState() {
+    GAME_STATE = GAME_STATE === 1 ? 2 : 1
+
+    requestAnimationFrame(update)
+}
+
+let lastTime = 0
+let elapsedTime = 0
+
+function update(timestamp) {
+    switch (GAME_STATE) {
+        case 1:
+            play(timestamp)
+            break
+        case 2:
+            //display pause screen
+            break
+        case 3:
+            GAME_STATE = 0
+            SCORE = 0
+            alert("GAME OVER!!")
+            break
+        default:
+            //display menu
+            break
+    }
+
     requestAnimationFrame(update)
 }
 
